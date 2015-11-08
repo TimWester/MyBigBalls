3eff6e56a51f69a3618d9e3b0de8a31de81881e5
TimWester <timretsew@gmail.com>
2015-11-08 16:15:51 +0100

    Initial commit

---
+++ Assets/Code/Input.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Input : MonoBehaviour {
+
+	public string Status;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello");
+	}
+	
+	// Update is called once per frame
+
+	void Update () {
+		//var a = GetComponent ();
+		var canon = gameObject;
+		canon.transform.Translate (new Vector3 (0.01f, 0.01f, 0.00f));
+
+	}
+
+
+void OnGUI() {
+		GUILayout.Label ( Status );
+	}
+
+}
